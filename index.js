00980c15649ded63b9a345deb1337fe531742978
Agustin <agusgibaut693@gmail.com>
2025-09-01 13:26:39 -0300

    termino seccion de inicio

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-document.getElementById('promo-btn').addEventListener('click', () => {
-    alert('Código aplicado: STYLE30');
+window.addEventListener('DOMContentLoaded', () => {
+    const banner = document.getElementById('cookie-banner');
+    const btnAccept = document.getElementById('accept-cookies');
+
+    // Mostrar banner si no se aceptó cookies
+    if (localStorage.getItem('cookiesAccepted') !== 'true') {
+        banner.classList.add('show');
+    }
+
+    // Aceptar cookies
+    btnAccept.addEventListener('click', () => {
+        banner.classList.remove('show');
+        localStorage.setItem('cookiesAccepted', 'true');
+    });
 });
